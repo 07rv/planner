e14107625813e64306ea3a16f135f8c83791dd3f
Rohit Verma <28vrohit@gmail.com>
2024-10-27 00:27:17 +0530

    hero

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,5 +1,22 @@
+import { Button } from "../ui/button";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex flex-col items-center mx-56 gap-9 mt-8">
+      <h1 className="font-extrabold text-3xl text-center">
+        <span className="text-orange-500">
+          Empower innovation by building with AI—
+        </span>
+        transform ideas into intelligent solutions, unlocking potential and
+        redefining what's possible in technology
+      </h1>
+      <p className="text-xl text-center text-gray-500">
+        Empower innovation by building with AI— transform ideas into intelligent
+        solutions, unlocking potential
+      </p>
+      <Button className="bg-blue-500">Get Started!</Button>
+    </div>
+  );
 };
 
 export default Hero;

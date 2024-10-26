a4401ed37ad040d1f7bc33dd4cae22d322895de2
Rohit Verma <28vrohit@gmail.com>
2024-10-26 22:34:04 +0530

    add header

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,5 +1,14 @@
+import { Button } from "../ui/button";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
+      <img className="w-10 h-10" src="/voice.svg" />
+      <div>
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
